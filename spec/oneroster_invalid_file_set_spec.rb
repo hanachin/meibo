57e1b0e5425798c537fb4bd017768ee29ca3e7a9
Seiei Miyagi <hanachin@gmail.com>
2022-11-04 09:56:08 +0900

    Add test for OneRoster TestSet

---
+++ spec/oneroster_invalid_file_set_spec.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+
+RSpec.describe 'OneRoster v1p2 CSV Conformance TestSet 20220707v1', skip: !Dir.exist?('tmp/TestSet') do
+  Dir.glob('tmp/TestSet/InvalidFileSet/Rostering/IRb0*.zip') do |path|
+    test_case = File.basename(path, '.zip')
+    describe test_case do
+      it 'raise error' do
+        expect { Meibo::Roster.from_file(path) }.to raise_error(Meibo::NotSupportedError)
+      end
+    end
+  end
+
+  Dir.glob('tmp/TestSet/InvalidFileSet/Rostering/IRb1*.zip') do |path|
+    test_case = File.basename(path, '.zip')
+    describe test_case do
+      it 'raise error (Missing Data)' do
+        expect { Meibo::Roster.from_file(path) }.to raise_error(Meibo::MissingDataError)
+      end
+    end
+  end
+
+  sourced_id_cases = %w[
+    IRb206
+    IRb211
+    IRb212
+    IRb213
+    IRb218
+    IRb219
+    IRb235
+    IRb236
+    IRb237
+    IRb246
+    IRb247
+    IRb248
+    IRb252
+    IRb255
+    IRb256
+    IRb261
+    IRb264
+  ]
+
+  Dir.glob('tmp/TestSet/InvalidFileSet/Rostering/IRb2*.zip') do |path|
+    test_case = File.basename(path, '.zip')
+    if sourced_id_cases.include?(test_case)
+      # NOTE: IRb247はリソースとの関連の検査
+      describe test_case, skip: test_case == 'IRb247' do
+        it 'raise error (Data Rules)' do
+          expect { Meibo::Roster.from_file(path) }.to raise_error(Meibo::DataNotFoundError)
+        end
+      end
+    else
+      describe test_case do
+        it 'raise error (Data Rules)' do
+          expect { Meibo::Roster.from_file(path) }.to raise_error(Meibo::InvalidDataTypeError)
+        end
+      end
+    end
+  end
+
+  Dir.glob('tmp/TestSet/InvalidFileSet/Rostering/IRb{3*,4[0-4]*}.zip') do |path|
+    test_case = File.basename(path, '.zip')
+    describe test_case do
+      it 'raise error' do
+        expect { Meibo::Roster.from_file(path) }.to raise_error(Meibo::MissingHeadersError)
+      end
+    end
+  end
+
+  Dir.glob('tmp/TestSet/InvalidFileSet/Rostering/IRb4[5-9]*.zip') do |path|
+    test_case = File.basename(path, '.zip')
+    describe test_case do
+      it 'raise error' do
+        expect { Meibo::Roster.from_file(path) }.to raise_error(Meibo::ScrambledHeadersError)
+      end
+    end
+  end
+
+  Dir.glob('tmp/TestSet/InvalidFileSet/Rostering/IRb5*.zip') do |path|
+    test_case = File.basename(path, '.zip')
+    describe test_case do
+      it 'raise error' do
+        expect { Meibo::Roster.from_file(path) }.to raise_error(Meibo::InvalidDataTypeError)
+      end
+    end
+  end
+end
---
+++ spec/oneroster_valid_file_set_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+RSpec.describe 'OneRoster v1p2 CSV Conformance TestSet 20220707v1', skip: !Dir.exist?('tmp/TestSet') do
+  Dir.glob('tmp/TestSet/ValidFileSet/Rostering/VRb*.zip') do |path|
+    test_case = File.basename(path, '.zip')
+    # NOTE: VRb157とVRb169はおそらくテストケース自体が間違い
+    describe test_case, skip: %w[VRb157 VRb169].include?(test_case) do
+      it 'does not raise error' do
+        expect { Meibo::Roster.from_file(path) }.not_to raise_error
+      end
+    end
+  end
+end
