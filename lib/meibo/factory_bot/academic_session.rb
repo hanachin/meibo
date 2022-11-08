ab69c053dc56f6f8f748f833da4eee72c8f4cef7
Seiei Miyagi <hanachin@gmail.com>
2022-11-08 17:14:04 +0900

    Fix attribute type

--- lib/meibo/factory_bot/academic_session.rb
+++ lib/meibo/factory_bot/academic_session.rb
@@ -15,8 +15,8 @@ FactoryBot.define do
     sourced_id { SecureRandom.uuid }
     title { "#{school_year}年度" }
     type { Meibo::AcademicSession::TYPES[:school_year] }
-    start_date { Date.new(school_year, 4, 1).iso8601 }
-    end_date { Date.new(school_year + 1, 3, 31).iso8601 }
+    start_date { Date.new(school_year, 4, 1) }
+    end_date { Date.new(school_year + 1, 3, 31) }
     parent_sourced_id { parent&.sourced_id }
     school_year { today.year }
 
