54f2d6b60b57eebd97a065a3d8716e38f665f93f
Seiei Miyagi <hanachin@gmail.com>
2026-06-02 16:27:21 +0900

    Support RubyZip v3

--- lib/meibo/roster.rb
+++ lib/meibo/roster.rb
@@ -132,8 +132,8 @@ module Meibo
       ].each(&:check_semantically_consistent)
     end
 
-    def write_to_buffer(io)
-      Zip::File.open_buffer(io) do |zipfile|
+    def write_to_buffer(io, create: true)
+      Zip::File.open_buffer(io, create:) do |zipfile|
         write(zipfile)
       end
     end
