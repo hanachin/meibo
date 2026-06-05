00eaec077c0d7a953c65ac046c48357aa349f34f
Seiei Miyagi <hanachin@gmail.com>
2026-06-05 18:27:32 +0900

    Add 全角バリデーション to フリガナ of preferred names

--- lib/meibo/eportal/v4/user.rb
+++ lib/meibo/eportal/v4/user.rb
@@ -5,7 +5,20 @@ module Meibo
     module V4
       class User < JapanProfile::User
         define_converters(
-          fullwidth: %i[given_name family_name middle_name preferred_given_name preferred_middle_name preferred_family_name kana_given_name kana_family_name kana_middle_name],
+          fullwidth: %i[
+            given_name
+            family_name
+            middle_name
+            preferred_given_name
+            preferred_middle_name
+            preferred_family_name
+            kana_given_name
+            kana_family_name
+            kana_middle_name
+            kana_preferred_family_name
+            kana_preferred_given_name
+            kana_preferred_middle_name
+          ],
           required: [*superclass.converters[:required], :user_master_identifier, :preferred_given_name, :preferred_family_name, :kana_given_name, :kana_family_name]
         )
       end
