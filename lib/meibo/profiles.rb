5cdadc996207b2d8078d90e5f5cc1ab073849c3a
Seiei Miyagi <hanachin@gmail.com>
2026-06-05 14:17:34 +0900

    Update profile names

--- lib/meibo/profiles.rb
+++ lib/meibo/profiles.rb
@@ -3,13 +3,13 @@
 module Meibo
   module Profiles
     PROFILES = {
-      "v1.2" => OneRoster::V1_2::PROFILE,
-      "v1.2.1" => OneRoster::V1_2_1::PROFILE,
-      "v1.2 ep v3.00" => Eportal::V3::PROFILE,
-      "v1.2 jp v1.1" => JapanProfile::V1_1::PROFILE,
-      "v1.2 jp v1.1.1" => JapanProfile::V1_1_1::PROFILE,
-      "v1.2.1 jp v1.2" => JapanProfile::V1_2::PROFILE,
-      "v1.2.1 ep v4.00" => Meibo::Eportal::V4::PROFILE,
+      "IMS OneRoster: CSV Binding Final Release v1.2" => OneRoster::V1_2::PROFILE,
+      "IMS OneRoster: CSV Binding Final Release v1.2.1" => OneRoster::V1_2_1::PROFILE,
+      "学習eポータル標準モデル Ver. 3.00" => Eportal::V3::PROFILE,
+      "OneRoster Japan Profile 第2版" => JapanProfile::V1_1::PROFILE,
+      "OneRoster Japan Profile 第2版 (rev.2)" => JapanProfile::V1_1_1::PROFILE,
+      "OneRoster Japan Profile v.1.2.1" => JapanProfile::V1_2::PROFILE,
+      "学習eポータル標準モデル Ver. 4.00" => Meibo::Eportal::V4::PROFILE,
       "初等中等教育におけるシステム間連携のための相互運用標準モデル Ver. 5.00" => Meibo::Eportal::V5::PROFILE
     }.freeze
 
