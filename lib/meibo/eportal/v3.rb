0c8e70f1d7f068238de50c19e277319da1d16e3c
Seiei Miyagi <hanachin@gmail.com>
2026-06-09 19:57:18 +0900

    Fix eportal v3.0

--- lib/meibo/eportal/v3.rb
+++ lib/meibo/eportal/v3.rb
@@ -3,8 +3,10 @@
 module Meibo
   module Eportal
     module V3
+      include ::Meibo::JapanProfile::V1_1
+
       PROFILE = Profile.new(
-        builders: ::Meibo::JapanProfile::V1_2::PROFILE.builders.merge(
+        builders: ::Meibo::JapanProfile::V1_1::PROFILE.builders.merge(
           class: Builder::ClassroomBuilder.create(::Meibo::Eportal::V3::Classroom),
           course: Builder::CourseBuilder.create(::Meibo::Eportal::V3::Course),
           enrollment: Builder::EnrollmentBuilder.create(::Meibo::Eportal::V3::Enrollment),
@@ -12,7 +14,7 @@ module Meibo
           user_profile: Builder::UserProfileBuilder.create(::Meibo::Eportal::V3::UserProfile),
           user: Builder::UserBuilder.create(::Meibo::Eportal::V3::User)
         ),
-        data_models: ::Meibo::JapanProfile::V1_2::PROFILE.data_models.merge(
+        data_models: ::Meibo::JapanProfile::V1_1::PROFILE.data_models.merge(
           file_classes: ::Meibo::Eportal::V3::Classroom,
           file_courses: ::Meibo::Eportal::V3::Course,
           file_enrollments: ::Meibo::Eportal::V3::Enrollment,
@@ -20,7 +22,7 @@ module Meibo
           file_user_profiles: ::Meibo::Eportal::V3::UserProfile,
           file_users: ::Meibo::Eportal::V3::User
         ),
-        data_set: ::Meibo::JapanProfile::V1_2::PROFILE.data_set,
+        data_set: ::Meibo::JapanProfile::V1_1::PROFILE.data_set,
         manifest_properties: { oneroster_version: "1.2" }
       )
     end
