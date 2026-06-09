f576fb7a1f0cc0890acfcb9e5bf027c88645a77e
Seiei Miyagi <hanachin@gmail.com>
2026-06-09 18:47:06 +0900

    Update Japan Profile v1.2 profile

--- lib/meibo/japan_profile/v1_2.rb
+++ lib/meibo/japan_profile/v1_2.rb
@@ -4,8 +4,8 @@ module Meibo
   module JapanProfile
     module V1_2
       PROFILE = Profile.new(
-        builders: V1_1::PROFILE.builders.merge(user: Builder::UserBuilder.create(User)),
-        data_models: V1_1::PROFILE.data_models.merge(file_users: User),
+        builders: V1_1::PROFILE.builders.merge(user: Builder::UserBuilder.create(::Meibo::JapanProfile::User)),
+        data_models: V1_1::PROFILE.data_models.merge(file_users: ::Meibo::JapanProfile::User),
         data_set: V1_1::PROFILE.data_set,
         manifest_properties: V1_1::PROFILE.manifest_properties
       )
