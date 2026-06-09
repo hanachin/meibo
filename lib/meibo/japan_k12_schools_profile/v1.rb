8e3d4abfb270ce5acb3c46c41cd6b3c368df1927
Seiei Miyagi <hanachin@gmail.com>
2026-06-09 18:48:45 +0900

    Update Japan K-12/Schools Profile v1.0 profile

--- lib/meibo/japan_k12_schools_profile/v1.rb
+++ lib/meibo/japan_k12_schools_profile/v1.rb
@@ -4,8 +4,12 @@ module Meibo
   module JapanK12SchoolsProfile
     module V1
       PROFILE = Profile.new(
-        builders: JapanProfile::V1_2::PROFILE.builders.merge(enrollment: Builder::EnrollmentBuilder.create(Enrollment)),
-        data_models: JapanProfile::V1_2::PROFILE.data_models.merge(file_enrollments: Enrollment),
+        builders: JapanProfile::V1_2::PROFILE.builders.merge(
+          enrollment: Builder::EnrollmentBuilder.create(::Meibo::JapanK12SchoolsProfile::Enrollment)
+        ),
+        data_models: JapanProfile::V1_2::PROFILE.data_models.merge(
+          file_enrollments: ::Meibo::JapanK12SchoolsProfile::Enrollment
+        ),
         data_set: JapanProfile::V1_2::PROFILE.data_set,
         manifest_properties: { oneroster_version: "1.2_JP" }
       )
