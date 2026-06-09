8f020b8bad890eb74fb6023d45e0034d337567e1
Seiei Miyagi <hanachin@gmail.com>
2026-06-09 19:57:50 +0900

    Update eporta v4.0

--- lib/meibo/eportal/v4.rb
+++ lib/meibo/eportal/v4.rb
@@ -3,12 +3,12 @@
 module Meibo
   module Eportal
     module V4
-      include V3
+      include ::Meibo::Eportal::V3
 
       PROFILE = Profile.new(
-        builders: V3::PROFILE.builders.merge(user: Builder::UserBuilder.create(::Meibo::Eportal::V4::User)),
-        data_models: V3::PROFILE.data_models.merge(file_users: ::Meibo::Eportal::V4::User),
-        data_set: V3::PROFILE.data_set,
+        builders: ::Meibo::Eportal::V3::PROFILE.builders.merge(user: Builder::UserBuilder.create(::Meibo::Eportal::V4::User)),
+        data_models: ::Meibo::Eportal::V3::PROFILE.data_models.merge(file_users: ::Meibo::Eportal::V4::User),
+        data_set: ::Meibo::Eportal::V3::PROFILE.data_set,
         manifest_properties: { oneroster_version: "1.2.1" }
       )
     end
